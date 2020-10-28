ee689410cf4d0a70b65257f961d9332d4d09792b
Chase Wohrle <chase.wohrle@gmail.com>
2020-10-27 19:48:21 -0700

    Commit10.27

--- index.js
+++ index.js
@@ -4,19 +4,37 @@
 function watchForm() {
   $('#js-form').submit(event => {
     event.preventDefault();
-    const numberOfParks = $('#search-qty').val();
-    console.log(numberOfParks);
-    if (!numberOfParks) {
-      getResults("10");
+    const state = $('#state').val();
+    console.log(state);
+    if (!state ) {
+      getResults('');
     } else {
-      getResults(numberOfParks);
+      getResults(state);
     }
   });
 }
 
-function getResults(numberOfParks) {
-  console.log(numberOfParks);
-  fetch(`https://developer.nps.gov/api/v1/parks?api_key=LjDGflPb5rpTne1YebbvKcvQv3UOczyGWZfXyJ0l&stateCode=ca&limit=${numberOfParks}`)
+/* 
+
+if locationtype is not all & location area is not all 
+  call specific endpoint for both 
+
+if locationtype is all & location area is  all 
+  call general endpoint for both   
+
+if location type is all & location area is not all
+  call general endpoint type and specific endpoint for area 
+
+if location type is not all & location area is all 
+  call specific endpoint for type and general endpoint for area
+
+
+*/
+
+
+function getResults(state) {
+  console.log(state);
+  fetch(`https://developer.nps.gov/api/v1/campgrounds?api_key=LjDGflPb5rpTne1YebbvKcvQv3UOczyGWZfXyJ0l&stateCode=${state}`)
     .then(response => {
       if (response.ok) {
         response.json().then(function(responseJson) {
@@ -25,7 +43,7 @@ function getResults(numberOfParks) {
       } 
     })
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-error-message').text(`An error has occurred: ${err.message}`);
     });
 }
 
@@ -35,9 +53,9 @@ function displayResults(responseJson) {
   let searchResults = responseJson['data']; 
   for (let i = 0; i < searchResults.length; i++) {
    $('#results-list').append(
-     `<li><h3>${searchResults[i].fullName}</h3>
+     `<li><h3>${searchResults[i].name}</h3>
       <p><a href="${searchResults[i].directionsUrl}">${searchResults[i].description}</a></p>
-      <p>${searchResults[i].url}</p>
+      <p>If available, a Campground Home URL will appear here:${searchResults[i].url}</p>
       </li>`
       )
   };
