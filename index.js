49c25acb092a05b02cae138fbf14955bd5066bf6
Chase Wohrle <chase.wohrle@gmail.com>
2020-10-30 21:47:56 -0700

    FIN

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function watchForm() {
   $('#js-form').submit(event => {
     event.preventDefault();
     const state = $('#state').val();
-    console.log(state);
     if (!state ) {
       getResults('');
     } else {
@@ -14,26 +13,8 @@ function watchForm() {
   });
 }
 
-/* 
-
-if locationtype is not all & location area is not all 
-  call specific endpoint for both 
-
-if locationtype is all & location area is  all 
-  call general endpoint for both   
-
-if location type is all & location area is not all
-  call general endpoint type and specific endpoint for area 
-
-if location type is not all & location area is all 
-  call specific endpoint for type and general endpoint for area
-
-
-*/
-
 
 function getResults(state) {
-  console.log(state);
   fetch(`https://developer.nps.gov/api/v1/campgrounds?api_key=LjDGflPb5rpTne1YebbvKcvQv3UOczyGWZfXyJ0l&stateCode=${state}`)
     .then(response => {
       if (response.ok) {
@@ -48,14 +29,13 @@ function getResults(state) {
 }
 
 function displayResults(responseJson) {
-  console.log(responseJson);
   $('#results-list').empty();
   let searchResults = responseJson['data']; 
   for (let i = 0; i < searchResults.length; i++) {
    $('#results-list').append(
      `<li><h3>${searchResults[i].name}</h3>
       <p><a href="${searchResults[i].directionsUrl}">${searchResults[i].description}</a></p>
-      <p>If available, a Campground Home URL will appear here:${searchResults[i].url}</p>
+      <p>If available, a Campground Home URL will appear here: ${searchResults[i].url}</p>
       </li>`
       )
   };
